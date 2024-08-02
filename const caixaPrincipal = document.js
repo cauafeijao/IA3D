67c32e95a7415d32e76ff1a9a3451ad67e2523e4
cauazinho_cm <110676745+cauafeijao@users.noreply.github.com>
2024-08-02 07:48:17 -0300

    Add files via upload

---
+++ const caixaPrincipal = document.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const perguntas = [// abre uma lista qualquer
+ {//abre o obijeto da lista (cada item)
+       enunciado: "pergunta 1",
+       alternativas :[
+           "alternativa 1"
+           "alternativa 2"
+       ]
+ }//fechar a o obijeto da lista(fin do iten)
+]//fechando a lista toda (completa)
